aef7094d507c83220df5e643254e66d2f0821dd1
徐杰 <2275817182@qq.com>
2024-06-02 10:58:46 +0800

    feat: set assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <h1 className="font-extrabold">HI</h1>
   );
 }
